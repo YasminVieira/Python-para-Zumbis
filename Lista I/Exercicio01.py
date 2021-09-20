41e5e07b0ec568131477b4325154d085556c67d3
Yasmin Vieira <yasmin.mima17@gmail.com>
2021-09-20 09:38:15 -0300

    Create Exercicio01.py

---
+++ Lista I/Exercicio01.py
@@ -0,0 +1,6 @@
+# Faça um programa que peça dois números inteiros e imprima a soma desses dois números
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite um outro número: '))
+
+print('A soma de {} + {} é igual a {}'.format(n1, n2, (n1+n2)))
