ad0b2eb40b56c67cab19fa2468e206d4457b15b1
Yasmin Vieira <yasmin.mima17@gmail.com>
2021-09-20 09:38:22 -0300

    Create Exercicio03.py

---
+++ Lista I/Exercicio03.py
@@ -0,0 +1,10 @@
+# Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário. Calcule o total em segundos
+
+d = int(input('Quantidade de dia(s): '))
+h = int(input('Quantidade de hora(s): '))
+m = int(input('Quantidade de minuto(s): '))
+s = int(input('Quantidade de segundo(s): '))
+
+conversor = s + (m*60) + (h*60*60) + (d*24*60*60)
+
+print('Convertendo tudo dá {} segundos'.format(conversor))
