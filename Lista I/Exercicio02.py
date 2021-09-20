404dcd78430df665279166927588724647fc75f2
Yasmin Vieira <yasmin.mima17@gmail.com>
2021-09-20 09:38:20 -0300

    Create Exercicio02.py

---
+++ Lista I/Exercicio02.py
@@ -0,0 +1,6 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em milímetros
+
+m = int(input('Digite um número em metros: '))
+mm = m * 1000
+
+print('{} metro(s) em milímetros vale {}'.format(m, mm))
