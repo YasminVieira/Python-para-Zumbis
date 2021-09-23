1405c3505b7d4fdfb3f2f0d48826c5f2a71df3dd
Yasmin Vieira <yasmin.mima17@gmail.com>
2021-09-23 10:52:43 -0300

    Create Exercicio04.py

---
+++ Lista I/Exercicio04.py
@@ -0,0 +1,7 @@
+# Faça um programa que calcule o aumento de um salário. Ele deve solicitar o valor do salário e a porcentagem do aumento. Exiba o valor do aumento e do novo salário
+
+salario = float(input('Qual o valor do salário: '))
+p_aumento = int(input('Qual a porcentagem do aumento: '))
+aumento = (p_aumento / 100) * salario
+print('O valor do aumento é de R$ {}'.format(aumento))
+print('O valor do novo salário com aumento é de R$ {}'.format(salario + aumento))
