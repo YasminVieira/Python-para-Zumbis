1b63d55981430bb2cd50ffbe806d990797bb7c25
Yasmin Vieira <yasmin.mima17@gmail.com>
2021-09-23 11:08:54 -0300

    Adding exercises from the first list

--- Lista I/Exercicio03.py
+++ Lista I/Exercicio03.py
@@ -1,4 +1,4 @@
-# Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário. Calcule o total em segundos
+# Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário. Calcule o total em segundos:
 
 d = int(input('Quantidade de dia(s): '))
 h = int(input('Quantidade de hora(s): '))
