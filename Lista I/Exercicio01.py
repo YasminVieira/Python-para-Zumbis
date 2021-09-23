8e892b4dfdb8bdfdb37b7707318f5b62cf3ad8a9
Yasmin Vieira <yasmin.mima17@gmail.com>
2021-09-23 11:04:59 -0300

    Create List I exercises

--- Lista I/Exercicio01.py
+++ Lista I/Exercicio01.py
@@ -1,4 +1,4 @@
-# Faça um programa que peça dois números inteiros e imprima a soma desses dois números
+# Faça um programa que peça dois números inteiros e imprima a soma desses dois números;
 
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite um outro número: '))
