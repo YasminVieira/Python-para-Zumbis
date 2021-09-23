623881f23943eff91fe66ac5755ee7d328b8c153
Yasmin Vieira <yasmin.mima17@gmail.com>
2021-09-23 11:11:28 -0300

    Adding exercises from the first list

--- Lista I/Exercicio04.py
+++ Lista I/Exercicio04.py
@@ -1,4 +1,4 @@
-# Faça um programa que calcule o aumento de um salário. Ele deve solicitar o valor do salário e a porcentagem do aumento. Exiba o valor do aumento e do novo salário
+# Faça um programa que calcule o aumento de um salário. Ele deve solicitar o valor do salário e a porcentagem do aumento. Exiba o valor do aumento e do novo salário:
 
 salario = float(input('Qual o valor do salário: '))
 p_aumento = int(input('Qual a porcentagem do aumento: '))
