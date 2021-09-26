bab1f887bb1945d82dc03fbe86a36c893e8e2cbb
Yasmin Vieira <yasmin.mima17@gmail.com>
2021-09-25 21:55:29 -0300

    Create Exercicio07.py

---
+++ Lista I/Exercicio07.py
@@ -0,0 +1,6 @@
+# Converta uma temperatura digitada em Celsius para Fahrenheit:
+# F = (9 * C / 5) + 32
+
+c = int(input('Digite um valor para temperatura em °C: '))
+f = (9 * c / 5) + 32
+print('{:.2f}°C equivale a {:.2f}°F'.format(c, f))
