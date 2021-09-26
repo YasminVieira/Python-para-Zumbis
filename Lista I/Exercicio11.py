fa7bd984bb547248ed07f212257fc83491adc99d
Yasmin Vieira <yasmin.mima17@gmail.com>
2021-09-25 23:03:20 -0300

    Adding exercises from the first list

---
+++ Lista I/Exercicio11.py
@@ -0,0 +1,6 @@
+# Sabendo que str() converte valores numéricos para string,
+# calcule quantos dígitos há em 2 elevado a um milhão:
+
+
+# A função len, quando aplicada a um string, retorna o número de caracteres no string (ou seja, o seu comprimento)
+print(len(str(2 ** 1000000)))
