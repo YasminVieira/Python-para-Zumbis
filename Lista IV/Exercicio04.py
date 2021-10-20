91915ba81194c3e202f130bba4b75a008c8a306c
Yasmin Vieira <yasmin.mima17@gmail.com>
2021-10-20 02:08:21 -0300

    Adding exercises from the fourth list

--- Lista IV/Exercicio04.py
+++ Lista IV/Exercicio04.py
@@ -6,13 +6,21 @@
 # os caracteres especiais e cuidado com maiúsculas e minúsculas:
 
 texto = '''The Python Software Foundation and the global Python
-community welcome and encou
-rage participation by everyone. Our community is based on
+community welcome and encourage participation by everyone. Our community is based on
 mutual respect, tolerance, and encouragement, and we are working to help each other live up
 to these principles. We want our community to be more diverse: whoever you are, and
-whatever your backgrou
-nd, we welcome you.'''
+whatever your background, we welcome you.'''
 texto = texto.lower()
 texto = texto.replace(',', ' ').replace('.', ' ').replace(':', ' ')
 texto = texto.split()
 print(texto)
+print()
+
+lista = []
+k = 0
+while k < len(texto):
+    x = texto[k]
+    if x[0] in 'python' or x[-1] in 'python':
+        lista.append(x)
+    k = k + 1
+print(lista)
