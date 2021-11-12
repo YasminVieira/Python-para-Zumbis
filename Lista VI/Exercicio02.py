331ffb4296386a0c8df24cd04c6414eda627e060
YasminVieira <yasmin.mima17@gmail.com>
2021-11-12 01:26:30 -0300

    Adding exercises from the sixth list

---
+++ Lista VI/Exercicio02.py
@@ -0,0 +1,35 @@
+# B. alunos_problema
+# temos dois alunos a e b
+# a_sorri e b_sorri indicam se a e b sorriem
+# temos problemas quando ambos estão sorrindo ou ambos não estão sorrindo
+# retorne True quando houver problemas
+def alunos_problema(a_sorri, b_sorri):
+    if a_sorri and b_sorri:
+        return True
+    if not a_sorri and not b_sorri:
+        return True
+    if a_sorri and not b_sorri:
+        return False
+    if not a_sorri and b_sorri:
+        return False
+
+# Provided simple test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(obtido, esperado):
+  if obtido == esperado:
+    prefixo = ' Parabéns!'
+  else:
+    prefixo = ' Ainda não'
+  print ('%s obtido: %s esperado: %s'
+         % (prefixo, repr(obtido), repr(esperado)))
+
+def main():
+  print ()
+  print ('Alunos problema')
+  test(alunos_problema(True, True), True)
+  test(alunos_problema(False, False), True)
+  test(alunos_problema(True, False), False)
+  test(alunos_problema(False, True), False)
+
+if __name__ == '__main__':
+  main()
